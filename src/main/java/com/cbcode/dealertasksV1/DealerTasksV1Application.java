ed7ac87ccb92a025ad5d594f084e9fd5c11e9746
cleber <lucleber13@hotmail.com>
2025-03-04 21:16:50 +0000

    Remove .gitignore file

---
+++ src/main/java/com/cbcode/dealertasksV1/DealerTasksV1Application.java
@@ -0,0 +1,13 @@
+package com.cbcode.dealertasksV1;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DealerTasksV1Application {
+
+    public static void main(String[] args) {
+        SpringApplication.run(DealerTasksV1Application.class, args);
+    }
+
+}
---
+++ src/test/java/com/cbcode/dealertasksV1/DealerTasksV1ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.cbcode.dealertasksV1;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DealerTasksV1ApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
